088dc0b0cf08d2c1625806e60bb45c5d7ffcb822
G284502022 <228450@st.takushoku-u.ac.jp>
2023-06-06 15:23:55 +0900

    project途中まで

--- project.js
+++ project.js
@@ -1,44 +1,96 @@
-let yosou = document.querySelector('button#kaitou');
-yosou.addEventListener('click', kensaku);
+let kensaku = document.querySelector('button#kensaku');
+let henkan = document.querySelector('button#henkan');
+kensaku.addEventListener('click', sendRequest);
+henkan.addEventListener('click', changeTemperature);
 
-function kensaku() {
+//セルシウス度
+let temperature = '℃';
+//華氏にするためのフラグ
+let flag = new Boolean(false);
+
+function sendRequest() {
+
     let e = document.querySelector('input[name="kensakuba-"]');
-    let Nyuuryokusitanaiyou = e.value;
+    let Nyuuryokusitanaiyou = Number(e.value);
 
-    let h2 = document.querySelector('h2#kensakukekka');
-    h2.textContent = Nyuuryokusitanaiyou+' の検索結果';
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+Nyuuryokusitanaiyou+'.json';
 
-    let p1 = document.querySelector('p#zyouhou0');
-    let p2 = document.querySelector('p#zyouhou1');
-    let p3 = document.querySelector('p#zyouhou2');
-    let p4 = document.querySelector('p#zyouhou3');
-    let p5 = document.querySelector('p#zyouhou4');
-    if(Nyuuryokusitanaiyou == 'エジプト'){
-        p1.textContent = 'カイロ';
-        p2.textContent = '天気:晴れ';
-        p3.textContent = '最高気温:47度';
-        p4.textContent = '最低気温:-3度';
-        p5.textContent = '湿度:89%';
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
+}
+
+let a = 0;
+
+function changeTemperature() {
+
+    if(a === 0){
+        temperature = '°F';
+        flag = true;
+        sendRequest();
+        a++;
+    }else if(a === 1){
+        temperature = '℃';
+        flag = false;
+        sendRequest();
+        a--;
     }
-    if(Nyuuryokusitanaiyou == 'ニホン'){
-        p1.textContent = '東京';
-        p2.textContent = '天気:雨';
-        p3.textContent = '最高気温:6000度';
-        p4.textContent = '最低気温:-10度';
-        p5.textContent = '湿度:29%';
+}
+// 通信が成功した時の処理
+function showResult(resp) {
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
+
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
     }
-    if(Nyuuryokusitanaiyou == 'ユナイテッドステイツ'){
-        p1.textContent = 'ニューヨーク';
-        p2.textContent = '天気:曇り';
-        p3.textContent = '最高気温:230°F';
-        p4.textContent = '最低気温:133°F';
-        p5.textContent = '湿度:42%';
+
+
+    if(flag === false){
+        data.main.temp = data.main.temp;
+        data.main.temp_max = data.main.temp_max;
+        data.main.temp_min = data.main.temp_min;
+    }else if(flag === true){
+        data.main.temp = (data.main.temp * 9 /5) + 32;
+        data.main.temp_max = (data.main.temp_max * 9 /5) + 32;
+        data.main.temp_min = (data.main.temp_min * 9 /5) + 32;
     }
-    if(Nyuuryokusitanaiyou == 'フランス'){
-        p1.textContent = 'パリ';
-        p2.textContent = '天気:晴れ';
-        p3.textContent = '最高気温:38度';
-        p4.textContent = '最低気温:21度';
-        p5.textContent = '湿度:22%';
-    }
+
+    let h2 = document.querySelector('h2#data_name');
+    let p0 = document.querySelector('p#data_weather');
+    let p1 = document.querySelector('p#temp');
+    let p2 = document.querySelector('p#temp_max');
+    let p3 = document.querySelector('p#temp_min');
+    let p4 = document.querySelector('p#humidity');
+    // データ表示
+    h2.textContent = data.name+'の検索結果';
+    p0.textContent = '天気:'+data.weather[0].main+'————'+data.weather[0].description;
+    p1.textContent = '気温:'+data.main.temp+temperature;
+    p2.textContent = '最高気温:'+data.main.temp_max+temperature;
+    p3.textContent = '最低気温:'+data.main.temp_min+temperature;
+    p4.textContent = '湿度:'+data.main.humidity+'%';
+    // data をコンソールに出力
+    console.log(data.name);
+    console.log(data.weather[0].main);
+    console.log(data.weather[0].description);
+
+    // data.x を出力
+    console.log(data.x);
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+    console.log(err);
+}
+
+function err(){
+    console.log('通信に失敗しました');
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+    console.log('Ajax 通信が終わりました');
 }
